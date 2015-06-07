1f52c225e8386a4eb2ed9b4da8b969d679849b19
Administrator <Administrator@LB51-20150117NG>
2015-06-07 23:14:16 +0800

    选课管理，没有做完。

---
+++ go_educate/WebRoot/script/attendance/elective.js
@@ -0,0 +1,13 @@
+/*
+ * 后退
+ */
+function back(){
+	location.href="findList.do"
+}
+/**
+ * 选课
+ * @param id
+ */
+function elective(id){
+	location.href="add.do?ID="+id;
+}
---
+++ go_educate/src/com/go/service/attendance/ElectiveService.java
@@ -0,0 +1,79 @@
+package com.go.service.attendance;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.go.po.common.PageBean;
+import com.go.service.base.BaseService;
+/**
+ * 选课Service
+ * @author Administrator
+ *
+ */
+@Service
+public class ElectiveService extends BaseService {
+
+	/**
+	 * 查询可选课时。
+	 * @param parameter
+	 * @return
+	 */
+	public List<Map<String,Object>> findOptionalLesson(Map<String,Object> parameter){
+		List<Map<String,Object>> list=this.getBaseDao().findList("elective.optionaldate", parameter);
+		for(Map<String,Object> map:list){
+			map.put("timelessonList", this.getBaseDao().findList("timelesson.findall",map));
+		}
+		return list;
+	}
+	/**
+	 * 分页查找数据
+	 * @param parameter
+	 * @return
+	 */
+	public  PageBean<Map<String,Object>>  findList(Map<String,Object> parameter){
+		return this.getBaseDao().findPageBean("elective.findcount", "elective.findlist", parameter);
+	}
+	/**
+	 * 根据条件查询所有数据
+	 * @param parameter
+	 * @return
+	 */
+	public List<Map<String,Object>> findAll(Map<String,Object> parameter){
+		return this.getBaseDao().findList("elective.findall", parameter);
+	}
+	
+	/**
+	 * 加载信息
+	 * @param parameter
+	 * @return
+	 */
+	public  Map<String,Object>  load(Map<String,Object> parameter){
+		return this.getBaseDao().loadEntity("elective.load", parameter);
+	}
+	/**
+	 * 添加菜单
+	 * @param parameter
+	 * @return
+	 */
+	public  void  add(Map<String,Object> parameter){
+	    this.getBaseDao().insert("elective.add", parameter);
+	}
+	
+	/**
+	 * 更新菜单
+	 * @param parameter
+	 */
+	public  void  update(Map<String,Object> parameter){
+		this.getBaseDao().update("elective.update", parameter);
+	}
+	
+	/**
+	 * 删除数据
+	 * @param parameter
+	 */
+	public  void  delete(List<String> parameter){
+		this.getBaseDao().delete("elective.delete", parameter);
+	}
+}
